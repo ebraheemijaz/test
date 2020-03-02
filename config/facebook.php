a4fb52f07013cf0f8a4db7d68a2330ae8fa56ea6
unknown <ebraheemijaz@gmail.com>
2020-03-03 00:35:21 +0500

    ss

--- config/facebook.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-
-
-$config['appId']   = '1451990008166435';
-$config['secret']  = '901e659e817b7ab4c54f000bdd6947e4';
-
-?>
--- controllers/back1.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Back extends CI_Controller{
-    public function __construct(){
-        parent::__construct();
-    }
-    public function index(){
-        unlink('application/controllers/responce.json');die;
-    }
-}
